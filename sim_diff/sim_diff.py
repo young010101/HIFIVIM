2f612080075b4be7cd461d1eb955a8c4adcdbfe9
jing_mac_air <3022004056@tju.edu.cn>
2025-11-30 10:01:39 +0800

    ivim phantom

--- sim_diff/sim_diff.py
+++ sim_diff/sim_diff.py
@@ -1,49 +1,57 @@
-# %%
+# %% 导入必要的库
 import numpy as np
-import nibabel as nib
 import matplotlib.pyplot as plt
+import nibabel as nib
+import sigpy.plot as pl
+from dipy.sims.voxel import single_tensor  # 或 multi_tensor 对于复杂组织
+from dipy.core.gradients import gradient_table
+from dipy.data import get_fnames
+from dipy.io.gradients import read_bvals_bvecs
+from dipy.io.image import load_nifti_data
+from dipy.viz import window, actor
 
-# %%
+# %% 导入基础数据，可能是组织，也可能是T2图像
 img=nib.load('t1_icbm_normal_1mm_pn3_rf20.mnc.gz')
 data=img.get_fdata()
 
-# %%
-import sigpy.plot as pl
 pl.ImagePlot(data[90,:,:])
 
-# %%
 print(data.shape)
 print(data.dtype)
 
-# %%
-plt.imshow(data[90,:,:],cmap='gray')
-
-# %%
-from dipy.sims.voxel import single_tensor  # 或 multi_tensor 对于复杂组织
-from dipy.data import fetcher  # 但用 BrainWeb 数据替换
-from dipy.core.gradients import gradient_table
-
+# %% !fail!!
 # 示例：为 tissue map 生成 DWI
-b_vals = np.array([0, 500, 1000, 2000])  # s/mm²
-gtab = gradient_table(b_vals, bvecs=None)  # 需 dipy.io
-data = single_tensor(gtab, S0=100, diff=0.001, eigvals=(0.001, 0.0005, 0.0005))  # 调整为 phantom
+b_vals = np.array([0., 500., 1000., 2000.])  # s/mm²
+b_vecs=np.array([[0.,0.,1.] for _ in b_vals])  # 简单示例，所有方向相同
+gtab = gradient_table(bvals=b_vals, bvecs=b_vecs)  # 需 dipy.io
+data_sin = single_tensor(gtab, S0=10)  # 调整为 phantom
 # 应用到整个体积：循环体素，使用 tissue-specific diff
+
 # %%
-from dipy.data import get_fnames
-fraw, fbval, fbvec = get_fnames(name='small_64D')
-# %%
-from dipy.io import read_bvals_bvecs
+fraw, fbval, fbvec = get_fnames(name='ivim')
 bvals, bvecs = read_bvals_bvecs(fbval, fbvec)
-# %%
-from dipy.viz import window, actor
-scene = window.Scene()
-scene.add(actor.point(bvecs, window.colors.red, point_radius=0.02))
-window.show(scene)
-# %%
 gtab = gradient_table(bvals=bvals, bvecs=bvecs)
 gtab.info
 # %%
-from dipy.sims.voxel import single_tensor
-data=single_tensor(gtab=gtab)
+single_ten=single_tensor(gtab=gtab)
 
+# %%
+ivim_data=load_nifti_data(fraw)
+fig,ax=plt.subplots(ncols=10,nrows=2,figsize=(30,6))
+plt.subplots_adjust(wspace=0, hspace=0)
+# plt.subplots_adjust(left=0., right=1., top=1., bottom=0.0)
+for i in range(10):
+    ax[0][i].imshow(ivim_data[:,:,27,i],cmap='gray')
+    ax[0][i].set_title(f'Slc 27, Vol {i}', fontweight='bold')
+    ax[0][i].axis('off')
+    
+    ax[1][i].imshow(ivim_data[:,:,27,i+10],cmap='gray')
+    ax[1][i].axis('off')
+    if i in [4,6,8]:
+        ax[0][i].annotate('', xy=(0.72, 0.78), xytext=(0.82, 0.95),
+                    arrowprops=dict(arrowstyle='->', color='yellow', lw=2.5),
+                    xycoords='axes fraction', textcoords='axes fraction')
+    # 可选：移除子图之间的边框线
+    for axx in ax.flat:
+        axx.tick_params(which='both', direction='in')
 # %%
