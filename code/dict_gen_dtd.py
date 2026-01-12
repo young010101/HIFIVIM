1544cb5f65ad49197ab7c69498e1717b2c0fada8
Cheng Yang <yc5@tju.edu.cn>
2026-01-12 22:49:41 +0800

    basis size

--- code/dict_gen_dtd.py
+++ code/dict_gen_dtd.py
@@ -76,13 +76,13 @@ def get_dicc2(s0, d_iso, mu2_iso, mu2_aniso, bvals, i):
     return i, signal, (s0, d_iso, mu2_iso, mu2_aniso)
 
 
-args = parser(["--outdir", "../Standard_Files_dtd"])
+args = parser(["--outdir", "../Standard_Files_dtd", "--basis_size", "3"])
 # bvals = np.asarray([0, 5, 7, 10, 15, 20, 30, 40, 50, 60, 100, 200, 400, 700, 1000, 1400, 2000])  # s/mm^2
 bvals = np.asarray([0, 7, 10, 15, 20, 40, 50, 60, 100, 200, 400, 700, 1000, 1400, 2000])  # s/mm^2
 
 size = 90
 s0 = 10
-d_iso = np.linspace(0.001, 4, size)  # 0, 0.4
+d_iso = np.linspace(0.1, 3.5, size)  # 0, 0.4
 mu2_iso = np.linspace(1e-6, 5, size)  # try it
 mu2_aniso = np.linspace(1e-6, 5, size)
 size = int(size * size * size)
@@ -101,7 +101,7 @@ with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
     results = np.asarray(results, dtype=object)
     for i in range(results.shape[0]):
         ivim_dicc[i] = results[i, 1]
-end_time = datetime.datetime.now()
+end_time = datetime.datetime.now() - start_time
 print("End time: ", end_time)
 
 # %% ensure dictionary validity
@@ -119,9 +119,7 @@ if args.make_basis:
     basis = bart(1, 'extract 1 0 {}'.format(args.basis_size), U)  # extract basis
     basis = bart(1, 'transpose 1 6', basis)  # place into correct bart format
     basis = bart(1, 'transpose 0 5', basis)
-# %%
     cfl.writecfl(os.path.join(args.outdir, 'dtd_bdelta1_basis_{}'.format(args.basis_size)), (basis))
-# %%
     # Visualize Dictionary and Basis
     if args.show_figure:
         plt.figure()
@@ -130,15 +128,19 @@ if args.make_basis:
         plt.xlabel('b-values [s/mm$^2$]', fontsize=22, fontweight='bold')
         plt.ylabel('Signal Intensity [AU]', fontsize=22, fontweight='bold')
 
-        plt.figure()
-        plt.subplot(121), plt.plot(abs(S1[:])), plt.scatter(np.arange(0, len(S1)), abs(S1[:]))
-        plt.xlabel('Principal Component', fontsize=22, fontweight='bold')
-        plt.ylabel('Percentage', fontsize=22, fontweight='bold')
+        fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+        ax = axes[0]
+        ax.plot(abs(S1[:]))
+        ax.scatter(np.arange(0, len(S1)), abs(S1[:]))
+        ax.set_xlabel('Principal Component', fontsize=22, fontweight='bold')
+        ax.set_ylabel('Percentage', fontsize=22, fontweight='bold')
 
-        plt.subplot(122), plt.plot(bvals, (np.squeeze(np.real(basis))))
-        plt.xlabel('b-values [s/mm$^2$]', fontsize=22, fontweight='bold')
-        plt.ylabel('Signal Intensity [AU]', fontsize=22, fontweight='bold')
-        plt.legend(['$\Phi_1$', '$\Phi_2$', '$\Phi_3$'], fontsize=18)
+        ax = axes[1]
+        ax.plot(bvals, (np.squeeze(np.real(basis))))
+        ax.set_xlabel('b-values [s/mm$^2$]', fontsize=22, fontweight='bold')
+        ax.set_ylabel('Signal Intensity [AU]', fontsize=22, fontweight='bold')
+        ax.legend(['$\Phi_1$', '$\Phi_2$', '$\Phi_3$'], fontsize=18)
+        fig.tight_layout()
         plt.show()
 # %%
 print("Basis set generation complete. Use dtd_bdelta1_basis_{} for LLR + Subspace reconstruction".format(
